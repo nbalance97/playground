877f0b6c4aeb62eb89fb366df31e99718a51f241
nbalance97 <bhlee971@gmail.com>
2023-08-20 13:20:15 +0900

    feat: github 레포지토리 테스트4

---
+++ lecture-spring-principle-adv/src/main/kotlin/com/example/lecturespringprincipleadv/LectureSpringPrincipleAdvApplication.kt
@@ -0,0 +1,11 @@
+package com.example.lecturespringprincipleadv
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LectureSpringPrincipleAdvApplication
+
+fun main(args: Array<String>) {
+    runApplication<LectureSpringPrincipleAdvApplication>(*args)
+}
---
+++ lecture-spring-principle-adv/src/test/kotlin/com/example/lecturespringprincipleadv/LectureSpringPrincipleAdvApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.lecturespringprincipleadv
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LectureSpringPrincipleAdvApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
